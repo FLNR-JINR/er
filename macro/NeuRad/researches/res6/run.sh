d0522bf2a82c3078f20ad12a216f9b70fc6bfc33
SchetininVitaliy <sch_vitaliy@mail.ru>
2016-01-09 10:25:23 +0300

    neurad researches

---
+++ macro/NeuRad/researches/res6/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nohup root -l -q 'res6.C()' &
---
+++ macro/NeuRad/sim/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#nohup root -l -q 'NeuRad_sim.C(100000, "sim500MeV100000evrc.root", "par500MeV100000evrc.root", 0.5)' &
+
+#nohup root -l -q 'NeuRad_sim.C(1000, "sim1.root", "par.root", 0.5)' &
+#nohup root -l -q 'NeuRad_digi.C(1000,"sim.root", "par.root", "digi.root")' &
+
+#nohup root -l -q 'NeuRad_digi.C(100000,"sim400MeV100000evrc.root", "par400MeV100000evrc.root", "digi400MeV100000evrc_woj.root")' &
+
+nohup root -l -q 'NeuRad_digi.C(100000,"sim500MeV100000evrc.root", "par500MeV100000evrc.root", "digi500MeV100000evrc_woj.root")' &
