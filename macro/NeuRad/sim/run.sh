bbe70682f27e08f0fad578c53f17976d65d4812c
SchetininVitaliy <sch_vitaliy@mail.ru>
2016-02-01 11:27:37 +0300

    run macro changes

--- macro/NeuRad/sim/run.sh
+++ macro/NeuRad/sim/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#nohup root -l -q 'NeuRad_sim.C(10000, "sim500MeV10000evrc.root", "par500MeV10000evrc.root", 0.5)' &
-#nohup root -l -q 'NeuRad_sim.C(10000, "sim400MeV10000evrc.root", "par400MeV10000evrc.root", 0.4)' &
+#nohup root -l -q 'NeuRad_sim.C(100000, "sim500MeV10000evrc.root", "par500MeV10000evrc.root", 0.5)' &
+#nohup root -l -q 'NeuRad_sim.C(100000, "sim400MeV10000evrc.root", "par400MeV10000evrc.root", 0.4)' &
 
-nohup root -l -q 'NeuRad_digi.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_onepe.root")' &
+nohup root -l -q 'NeuRad_digi_full.C(100000,"sim500MeV100000evrc.root", "par500MeV100000evrc.root", "digi500MeV100000evpe.root")' &
+nohup root -l -q 'NeuRad_digi_full.C(100000,"sim400MeV100000evrc.root", "par400MeV100000evrc.root", "digi400MeV100000evpe.root")' &
 
 #nohup root -l -q 'NeuRad_digi_DT.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_DT0001.root", 0.001)' &
 #nohup root -l -q 'NeuRad_digi_DT.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_DT001.root", 0.01)' &
