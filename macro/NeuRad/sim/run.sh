58b464c8f8f7ece789a0316f59bb70eccf46eb7f
SchetininVitaliy <sch_vitaliy@mail.ru>
2016-02-01 18:45:13 +0300

    x

--- macro/NeuRad/sim/run.sh
+++ macro/NeuRad/sim/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-#nohup root -l -q 'NeuRad_sim.C(100000, "sim500MeV10000evrc.root", "par500MeV10000evrc.root", 0.5)' &
-#nohup root -l -q 'NeuRad_sim.C(100000, "sim400MeV10000evrc.root", "par400MeV10000evrc.root", 0.4)' &
+#nohup root -l -q 'NeuRad_sim.C(10000, "sim500MeV10000evrcv2.root", "par500MeV10000evrcv2.root", 0.5)' &
+#nohup root -l -q 'NeuRad_sim.C(10000, "sim400MeV10000evrcv2.root", "par400MeV10000evrcv2.root", 0.4)' &
 
-<<<<<<< HEAD
 #nohup root -l -q 'NeuRad_digi.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_onepe.root")' &
 #nohup root -l -q 'NeuRad_digi_full.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe.root")' &
 #nohup root -l -q 'NeuRad_digi_full.C(10000,"sim400MeV10000evrc.root", "par400MeV10000evrc.root", "digi400MeV10000evpe.root")' &
-=======
-nohup root -l -q 'NeuRad_digi_full.C(100000,"sim500MeV100000evrc.root", "par500MeV100000evrc.root", "digi500MeV100000evpe.root")' &
-nohup root -l -q 'NeuRad_digi_full.C(100000,"sim400MeV100000evrc.root", "par400MeV100000evrc.root", "digi400MeV100000evpe.root")' &
 
->>>>>>> bbe70682f27e08f0fad578c53f17976d65d4812c
+#nohup root -l -q 'NeuRad_digi_full.C(100000,"sim500MeV100000evrc.root", "par500MeV100000evrc.root", "digi500MeV100000evpe.root")' &
+#nohup root -l -q 'NeuRad_digi_full.C(100000,"sim400MeV100000evrc.root", "par400MeV100000evrc.root", "digi400MeV100000evpe.root")' &
+
 #nohup root -l -q 'NeuRad_digi_DT.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_DT0001.root", 0.001)' &
 #nohup root -l -q 'NeuRad_digi_DT.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_DT001.root", 0.01)' &
 #nohup root -l -q 'NeuRad_digi_DT.C(10000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evrc_DT01.root", 0.1)' &
@@ -23,11 +21,17 @@ nohup root -l -q 'NeuRad_digi_full.C(100000,"sim400MeV100000evrc.root", "par400M
 #nohup root -l -q 'NeuRad_digi.C(100000,"sim400MeV100000evrc.root", "par400MeV100000evrc.root", "digi400MeV100000evrc_woj.root")' &
 
 #nohup root -l -q 'NeuRad_digi.C(100000,"sim500MeV100000evrc.root", "par500MeV100000evrc.root", "digi500MeV100000evrc_woj.root")' &
-<<<<<<< HEAD
 
-#nohup root -l -q 'NeuRad_digi_full.C(0,5,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_0_5.root")' &
-#nohup root -l -q 'NeuRad_digi_full.C(5,10,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_5_10.root")' &
+nohup root -l -q 'NeuRad_digi.C(0,1000,"sim500MeV10000evrcv2.root", "par500MeV10000evrcv2.root", "digi500MeV10000evpe_0_1000.root")' &
+nohup root -l -q 'NeuRad_digi.C(1000,2000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_1000_2000.root")' &
+nohup root -l -q 'NeuRad_digi.C(2000,3000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_2000_3000.root")' &
+nohup root -l -q 'NeuRad_digi.C(3000,4000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_3000_4000.root")' &
+nohup root -l -q 'NeuRad_digi.C(4000,5000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_4000_5000.root")' &
+nohup root -l -q 'NeuRad_digi.C(5000,6000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_5000_6000.root")' &
+nohup root -l -q 'NeuRad_digi.C(6000,7000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_6000_7000.root")' &
+nohup root -l -q 'NeuRad_digi.C(6000,7000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_7000_8000.root")' &
+nohup root -l -q 'NeuRad_digi.C(6000,7000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_8000_9000.root")' &
+nohup root -l -q 'NeuRad_digi.C(6000,7000,"sim500MeV10000evrc.root", "par500MeV10000evrc.root", "digi500MeV10000evpe_9000_10000.root")' &
 
-nohup root -l -q 'merge.C("digi500MeV10000evpe_0_5.root", "digi500MeV10000evpe_5_10.root", "digi500MeV10evpe.root")' 
-=======
->>>>>>> bbe70682f27e08f0fad578c53f17976d65d4812c
+#nohup root -l -q 'merge.C("digi500MeV10000evpe_0_1000.root,digi500MeV10000evpe_1000_2000.root,digi500MeV10000evpe_2000_3000.root,digi500MeV10000evpe_3000_4000.root,digi500MeV10000evpe_4000_5000.root,digi500MeV10000evpe_5000_6000.root,digi500MeV10000evpe_6000_7000.root,digi500MeV10000evpe_7000_8000.root,digi500MeV10000evpe_8000_9000.root,digi500MeV10000evpe_9000_10000.root","digi500MeV10evpe.root")' &
+
