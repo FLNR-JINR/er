f416bef0bb93a001f272bff958fb67395ca1e86b
SchetininVitaliy <sch_vitaliy@mail.ru>
2016-03-07 08:49:57 +0300

    ERGammaGenerator

---
+++ generators/ERGammaGenerator.cxx
@@ -0,0 +1,64 @@
+
+
+#include "ERGammaGenerator.h"
+#include "TMath.h"
+#include "FairLogger.h"
+
+#include "G4IonTable.hh"
+#include "G4ParticleTable.hh"
+
+using namespace std;
+
+ERGammaGenerator::ERGammaGenerator():
+fPhiMinCM(0.),
+fPhiMaxCM(0.),
+fThetaMinCM(0.),
+fThetaMaxCM(0.),
+fEBeam(0.)
+{
+	fRnd = new TRandom3();
+}
+
+ERGammaGenerator::~ERGammaGenerator(){
+
+}
+
+Bool_t ERGammaGenerator::ReadEvent(FairPrimaryGenerator* primGen) {
+	Double_t phiCM = fRnd->Uniform()*(fPhiMaxCM - fPhiMinCM) + fPhiMinCM;
+	phiCM *= TMath::DegToRad();
+	fThetaMinCM *= TMath::DegToRad();
+	fThetaMaxCM *= TMath::DegToRad();
+
+	G4IonTable* fIonTable =  G4IonTable::GetIonTable(); 
+  	G4ParticleTable* fParticleTable = G4ParticleTable::GetParticleTable();
+  	fGammaPDG = fParticleTable->FindParticle("gamma")->GetPDGEncoding();
+	fIonMass = fIonTable->GetIonMass(fZ,fA)/1000.;
+	cerr << "fEBeam = " << fEBeam << endl;
+	Double_t fullEBeam = fEBeam + fIonMass;
+	Double_t cos_thetaCM = fRnd->Uniform(-1., 1.);
+	Double_t sin_thetaCM = TMath::Sqrt(1-cos_thetaCM*cos_thetaCM);
+	cerr << "Ion mass=" << fIonMass << " Full E beam = " << fullEBeam << endl;
+	Double_t pGammaCM = fEGammaCM; 
+	cerr << "Theta CM = " << TMath::ACos(cos_thetaCM)*TMath::RadToDeg()  << "[Deg]"<< endl;
+	Double_t pxCM = pGammaCM*TMath::Cos(phiCM)*sin_thetaCM;
+	Double_t pyCM = pGammaCM*TMath::Sin(phiCM)*sin_thetaCM;
+	Double_t pzCM = pGammaCM*cos_thetaCM;
+	cerr << "pxCM = " << pxCM << " pyCM = " << pyCM << " pzCM = " << pzCM << endl;
+
+	Double_t pBeam = TMath::Sqrt(fullEBeam*fullEBeam - fIonMass*fIonMass);
+	Double_t betaCM = pBeam/fullEBeam;
+	Double_t gammaCM = TMath::Sqrt( 1. / ( 1. - betaCM*betaCM) );
+
+	Double_t pxLAB = pxCM;
+	Double_t pyLAB = pyCM;
+	Double_t pzLAB = gammaCM * ( pzCM + betaCM * fEGammaCM);
+	cerr << "pxLAB = " << pxLAB << " pyLAB = " << pyLAB << " pzLAB = " << pzLAB << endl;
+	cerr << "gammaCM = " << gammaCM << " betaCM = " << betaCM << endl;
+
+	Double_t pLAB = TMath::Sqrt(pxLAB*pxLAB + pyLAB*pyLAB + pzLAB*pzLAB);
+	cerr << "Theta Lab = " << TMath::ACos(pzLAB/pLAB)*TMath::RadToDeg()  << "[Deg]"<< endl;
+	primGen->AddTrack(fGammaPDG, pxLAB, pyLAB, pzLAB,0, 0, 0);
+
+	return kTRUE;
+}
+
---
+++ generators/ERGammaGenerator.h
@@ -0,0 +1,66 @@
+// -------------------------------------------------------------------------
+// -----                ERGammaGenerator header file                   -----
+// -----          Created 02/14  by V.Schetinin                        -----
+// -------------------------------------------------------------------------
+
+#ifndef ERGAMMAGENERATOR_H
+#define ERGAMMAGENERATOR_H 1
+
+
+#include "FairGenerator.h"
+#include "FairPrimaryGenerator.h"
+
+#include "TRandom3.h"
+
+#include <iostream>
+
+//using namespace std;
+
+
+class ERGammaGenerator : public FairGenerator
+{
+
+ public:
+
+  /** Default constructor without arguments should not be used. **/
+  ERGammaGenerator();
+
+  /** Destructor. **/
+  virtual ~ERGammaGenerator();
+
+  /** Reads on event from the input file and pushes the tracks onto
+   ** the stack. Abstract method in base class.
+   ** @param pStack    pointer to the stack
+   ** @param ver       not used
+   **/
+  virtual Bool_t ReadEvent(FairPrimaryGenerator* primGen);
+  void SetBeamEnergy(Double_t en){fEBeam = en;}
+  void SetGammaCMEnergy(Double_t en){fEGammaCM = en;}
+  void SetGammaCMPhiRange(Double_t phiMin, Double_t phiMax){fPhiMinCM = phiMin; fPhiMaxCM= phiMax;}
+  void SetGammaCMThetaRange(Double_t thetaMin, Double_t thetaMax){fThetaMinCM = thetaMin; fThetaMaxCM= thetaMax; }
+  void SetIon(Int_t z, Int_t a){fA = a; fZ = z;}
+
+  //Skip some events in file
+  Bool_t SkipEvents(Int_t count);
+   
+ private:
+  Int_t fA;
+  Int_t fZ;
+  Double_t fEBeam;
+  Double_t fEGammaCM;
+  Double_t fPhiMinCM, fPhiMaxCM;
+  Double_t fThetaMinCM, fThetaMaxCM;
+  Double_t fIonMass;
+  Int_t fGammaPDG;
+
+  TRandom3 *fRnd;
+
+  ERGammaGenerator(const ERGammaGenerator&);
+  ERGammaGenerator& operator=(const ERGammaGenerator&);
+
+  ClassDef(ERGammaGenerator,1);
+ 
+};
+
+#endif
+   
