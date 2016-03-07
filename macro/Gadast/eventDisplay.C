6fb0d0752dd9090d6200f9c1049c1d0d764b6e0a
SchetininVitaliy <sch_vitaliy@mail.ru>
2016-03-07 08:52:35 +0300

    add gadast event display

---
+++ macro/Gadast/eventDisplay.C
@@ -0,0 +1,24 @@
+eventDisplay()
+{
+  FairRunAna *fRun= new FairRunAna();
+  
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open("par.root");
+  rtdb->setFirstInput(parIo1);
+  //rtdb->print();
+  
+  fRun->SetInputFile("sim.root");
+  fRun->SetOutputFile("EventDisplay.root");
+  
+  FairEventManager *fMan= new FairEventManager();
+  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
+  FairMCPointDraw *CsIPoints =   new FairMCPointDraw ("GadastCsIPoint",kOrange,  kFullSquare);
+  FairMCPointDraw *LaBrPoints =   new FairMCPointDraw ("GadastLaBrPoint",kOrange,  kFullSquare);
+  
+  fMan->AddTask(Track);
+  fMan->AddTask(CsIPoints);
+  fMan->AddTask(LaBrPoints);
+  
+  fMan->Init();
+}
