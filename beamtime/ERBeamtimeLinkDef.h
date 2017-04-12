5c3f80dc056207a4a8890158651a3c38be5a4787
ivan.muzalevsky <ivanmuzalevskij@gmail.com>
2017-04-12 10:54:14 +0300

    makefiles for new class

--- beamtime/ERBeamtimeLinkDef.h
+++ beamtime/ERBeamtimeLinkDef.h
@@ -7,6 +7,7 @@
 #pragma link C++ class ERDRS4Source+;
 #pragma link C++ class ERTektronixSource+;
 #pragma link C++ class ERRawToAnalyzeConverter+;
+#pragma link C++ class ERSimtoRaw+;
 
 #pragma link C++ class ERLiCalibrator+;
 #pragma link C++ class ERRootSource+;
@@ -14,5 +15,6 @@
 #pragma link C++ class ERNXyterTreeSource+;
 #pragma link C++ class ERNXyterCalibrator+;
 #pragma link C++ class ERpmtPixelMap+;
+
 
 #endif
