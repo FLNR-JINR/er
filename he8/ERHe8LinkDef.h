fdbedc9f21fefe0608bc283d8cc6e8318ddc80bc
SchetininVitaliy <sch_vitaliy@mail.ru>
2017-05-26 10:03:47 +0300

    change directory structure

--- he8/ERHe8LinkDef.h
+++ he8/ERHe8LinkDef.h
@@ -5,27 +5,23 @@
 #pragma link off all functions;
 
 #pragma link C++ class ERRawEvent+;
-#pragma link C++ class ERBeamDetEvent+;
-#pragma link C++ class ERBeamDetCalEvent+;
-#pragma link C++ class ERTelescopeEvent+;
-#pragma link C++ class ERDsrdEvent+;
 #pragma link C++ class ERHe8EventHeader+;
-#pragma link C++ class ERDsrdCalEvent+;
-#pragma link C++ class ERTelescopeCalEvent+;
-
-#pragma link C++ class ERDsrdCalibrator+;
-#pragma link C++ class ERTelescopeCalibrator+;
-#pragma link C++ class ERBeamDetCalibrator+;
-
 #pragma link C++ class ERHe8Analyzer+;
 #pragma link C++ class ERTelData+;
 #pragma link C++ class ERInclusiveData+;
-#pragma link C++ class RawTrack+;
-#pragma link C++ class TrackData+;
+#pragma link C++ class ERParticle+;
 
+#pragma link C++ class ERBeamDetEvent+;
+#pragma link C++ class ERBeamDetCalEvent+;
 #pragma link C++ class ERBeamDetRecoEvent+;
+#pragma link C++ class ERBeamDetCalibrator+;
 #pragma link C++ class ERBeamDetReconstructor+;
 
-#pragma link C++ class ERParticle+;
+#pragma link C++ class ERTelescopeEvent+;
+#pragma link C++ class ERTelescopeCalEvent+;
+#pragma link C++ class ERDsrdEvent+;
+#pragma link C++ class ERDsrdCalEvent+;
+#pragma link C++ class ERTelescopeCalibrator+;
+#pragma link C++ class ERDsrdCalibrator+;
 
 #endif
