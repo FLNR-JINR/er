ca7c26e58e82ed9a6a025402e730de72250358ae
Olegunt <chernov.oleg2009@yandex.ru>
2017-07-05 17:45:59 +0300

    Update ERRTelescopeSetup.cxx

--- RTelescope/ERRTelescopeSetup.cxx
+++ RTelescope/ERRTelescopeSetup.cxx
@@ -7,7 +7,7 @@
 #include "TMath.h"
 
 ERRTelescopeSetup* ERRTelescopeSetup::fInstance = NULL;
-TGeoNode* ERRTelescopeSetup::fDSRDnode = NULL;
+TGeoNode* ERRTelescopeSetup::fRTelescopenode = NULL;
 Float_t ERRTelescopeSetup::fZ = 0;
 Float_t ERRTelescopeSetup::fRmin = 0;
 Float_t ERRTelescopeSetup::fRmax = 0;
@@ -21,17 +21,17 @@ ERRTelescopeSetup::ERRTelescopeSetup(){
     }
     gGeoManager->CdTop();
     TGeoNode* cave = gGeoManager->GetCurrentNode();
-    fDSRDnode  = NULL;
+    fRTelescopenode  = NULL;
     for (Int_t iNode = 0; iNode < cave->GetNdaughters(); iNode++) {
         TString name = cave->GetDaughter(iNode)->GetName();
-        if ( name.Contains("DSRD", TString::kIgnoreCase) ) {
-            fDSRDnode = cave->GetDaughter(iNode);
+        if ( name.Contains("RTelescope", TString::kIgnoreCase) ) {
+            fRTelescopenode = cave->GetDaughter(iNode);
         }
     }
-    fZ = fDSRDnode->GetMatrix()->GetTranslation()[2];
-    fSecNb = fDSRDnode->GetNdaughters();
+    fZ = fRTelescopenode->GetMatrix()->GetTranslation()[2];
+    fSecNb = fRTelescopenode->GetNdaughters();
     
-    TGeoNode* sector = fDSRDnode->GetDaughter(0);
+    TGeoNode* sector = fRTelescopenode->GetDaughter(0);
     TGeoSphere* sector_shape = (TGeoSphere*)sector->GetVolume()->GetShape();
 
     fRmin = TMath::Tan(sector_shape->GetTheta1()*TMath::DegToRad())*sector_shape->GetRmin();
@@ -47,7 +47,7 @@ ERRTelescopeSetup* ERRTelescopeSetup::Instance(){
 }
 
 void ERRTelescopeSetup::Print(){
-    std::cout << "DSRD Z position:" << fZ << std::endl;
-    std::cout << "DSRD Rmin, Rmax:" << fRmin << "," << fRmax << std::endl;
-    std::cout << "DSRD SectorNb, SensorNb:" << fSecNb << "," << fSenNb << std::endl;
+    std::cout << "RTelescope Z position:" << fZ << std::endl;
+    std::cout << "RTelescope Rmin, Rmax:" << fRmin << "," << fRmax << std::endl;
+    std::cout << "RTelescope SectorNb, SensorNb:" << fSecNb << "," << fSenNb << std::endl;
 }
