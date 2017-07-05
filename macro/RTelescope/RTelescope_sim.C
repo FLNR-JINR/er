782be479abb9c85eee65a623fe53aebd1d14183c
Olegunt <chernov.oleg2009@yandex.ru>
2017-07-05 15:56:19 +0300

    Update RTelescope_sim.C

--- macro/RTelescope/RTelescope_sim.C
+++ macro/RTelescope/RTelescope_sim.C
@@ -40,7 +40,7 @@ void RTelescope_sim(Int_t nEvents = 1){
   */
   Int_t verbose = 1;
   ERRTelescope* RTelescope= new ERRTelescope("ERRTelescope", kTRUE,verbose);
-  RTelescope->SetGeometryFileName("RTelescope.v1.geo.root");
+  RTelescope->SetGeometryFileName("RTelescope.v2.geo.root");
   run->AddModule(RTelescope);
   // ------------------------------------------------------------------------
 	
@@ -48,14 +48,14 @@ void RTelescope_sim(Int_t nEvents = 1){
   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
   Int_t pdgId = 2212; // proton  beam
   Double32_t theta1 = 150;  // polar angle distribution
-  Double32_t theta2 = 150;
+  Double32_t theta2 = 220;
   Double32_t kin_energy = .5; //GeV
   Double_t mass = TDatabasePDG::Instance()->GetParticle(pdgId)->Mass();
   Double32_t momentum = TMath::Sqrt(kin_energy*kin_energy + 2.*kin_energy*mass); //GeV
   FairBoxGenerator* boxGen = new FairBoxGenerator(pdgId, 1);
   boxGen->SetThetaRange(theta1, theta2);
   boxGen->SetPRange(momentum, momentum);
-  boxGen->SetPhiRange(90., 90.);
+  boxGen->SetPhiRange(0., 360.);
   boxGen->SetBoxXYZ(0.,0,0.0,0.0,0.);
 
   primGen->AddGenerator(boxGen);
