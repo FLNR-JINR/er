0c805d189665cc7e1a18ed97c58ca83191fc7f2b
SchetininVitaliy <sch_vitaliy@mail.ru>
2017-07-07 16:40:33 +0300

    merge beamtests analisys and sim of he8 and S28 detectors

--- BeamDet/ERBeamDetLinkDef.h
+++ BeamDet/ERBeamDetLinkDef.h
@@ -5,7 +5,13 @@
 #pragma link off all functions;
 
 #pragma link C++ class ERBeamDet;
+#pragma link C++ class ERBeamDetCalibrator;
+#pragma link C++ class ERBeamDetReconstructor;
+
 #pragma link C++ class ERBeamDetTOFPoint+;
 #pragma link C++ class ERBeamDetMWPCPoint+;
+#pragma link C++ class ERBeamDetEvent+;
+#pragma link C++ class ERBeamDetCalEvent+;
+#pragma link C++ class ERBeamDetRecoEvent+;
 
 #endif
