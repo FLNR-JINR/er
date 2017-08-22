6d51d20f5de2d7a76b872c627e9fec648b170bd6
bobrikula <bobrikula@mail.ru>
2017-08-22 17:34:33 +0300

    added new variables at CsIPoint and SiPoint named fTelescopeNb and fDetectorNb SiDigi finished (I will change it a little bit when the geometry will contains 2 telescopes)

--- macro/RTelescope/RTelescope_digi.C
+++ macro/RTelescope/RTelescope_digi.C
@@ -1,58 +1,59 @@
-void RTelescope_digi(Int_t nEvents = 1000){
-  //---------------------Files-----------------------------------------------
-  TString inFile = "sim.root";
-  TString outFile = "digi.root";
-  TString parFile = "par.root";
-  TString parOutFile = "parOut.root";
-  // ------------------------------------------------------------------------
-  // -----   Timer   --------------------------------------------------------
-  TStopwatch timer;
-  timer.Start();
-  // ------------------------------------------------------------------------
+void RTelescope_digi(Int_t nEvents = 1000)
+{
+    //---------------------Files-----------------------------------------------
+    TString inFile = "sim.root";
+    TString outFile = "digi.root";
+    TString parFile = "par.root";
+    TString parOutFile = "parOut.root";
+    // ------------------------------------------------------------------------
+    // -----   Timer   --------------------------------------------------------
+    TStopwatch timer;
+    timer.Start();
+    // ------------------------------------------------------------------------
   
-  // -----   Digitization run   -------------------------------------------
-  FairRunAna *fRun= new FairRunAna();
-  fRun->SetInputFile(inFile);
-  fRun->SetOutputFile(outFile);
-  // ------------------------------------------------------------------------
-  //-------- Set MC event header --------------------------------------------
-  EREventHeader* header = new EREventHeader();
-  fRun->SetEventHeader(header);
-  //------------------------------------------------------------------------
-  // ------------------------NeuRadDigitizer---------------------------------
-  Int_t verbose = 1; // 1 - only standard log print, 2 - print digi information 
-  ERRTelescopeDigitizer* digitizer = new ERRTelescopeDigitizer(verbose);
-  fRun->AddTask(digitizer);
-  // ------------------------------------------------------------------------
+    // -----   Digitization run   -------------------------------------------
+    FairRunAna *fRun= new FairRunAna();
+    fRun->SetInputFile(inFile);
+    fRun->SetOutputFile(outFile);
+    // ------------------------------------------------------------------------
+    //-------- Set MC event header --------------------------------------------
+    EREventHeader* header = new EREventHeader();
+    fRun->SetEventHeader(header);
+    //------------------------------------------------------------------------
+    // ------------------------NeuRadDigitizer---------------------------------
+    Int_t verbose = 1; // 1 - only standard log print, 2 - print digi information 
+    ERRTelescopeDigitizer* digitizer = new ERRTelescopeDigitizer(verbose);
+    fRun->AddTask(digitizer);
+    // ------------------------------------------------------------------------
   
-  // -----------Runtime DataBase info -------------------------------------
-  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+    // -----------Runtime DataBase info -------------------------------------
+    FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
 
-  FairParRootFileIo*  parInput = new FairParRootFileIo();
-  parInput->open(parFile.Data(), "UPDATE");
+    FairParRootFileIo*  parInput = new FairParRootFileIo();
+    parInput->open(parFile.Data(), "UPDATE");
   
-  rtdb->setFirstInput(parInput);
+    rtdb->setFirstInput(parInput);
   
-  // -----   Intialise and run   --------------------------------------------
-  FairLogger::GetLogger()->SetLogScreenLevel("INFO");
-  fRun->Init();
-  fRun->Run(0, nEvents);
-  // ------------------------------------------------------------------------
-  //FairParRootFileIo*  parIo2 = new FairParRootFileIo();
-  //parIo2->open(parOutFile.Data());
-  rtdb->setOutput(parInput);
-  rtdb->saveOutput();
+    // -----   Intialise and run   --------------------------------------------
+    FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+    fRun->Init();
+    fRun->Run(0, nEvents);
+    // ------------------------------------------------------------------------
+    //FairParRootFileIo*  parIo2 = new FairParRootFileIo();
+    //parIo2->open(parOutFile.Data());
+    rtdb->setOutput(parInput);
+    rtdb->saveOutput();
   
-  // -----   Finish   -------------------------------------------------------
-  timer.Stop();
-  Double_t rtime = timer.RealTime();
-  Double_t ctime = timer.CpuTime();
-  cout << endl << endl;
-  cout << "Macro finished succesfully." << endl;
-  cout << "Output file writen:  "    << outFile << endl;
-  cout << "Parameter file writen " << parFile << endl;
-  cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
-  cout << endl;
-  // ------------------------------------------------------------------------
-
+    // -----   Finish   -------------------------------------------------------
+    timer.Stop();
+    Double_t rtime = timer.RealTime();
+    Double_t ctime = timer.CpuTime();
+    
+    cout << endl << endl;
+    cout << "Macro finished succesfully." << endl;
+    cout << "Output file writen:  "    << outFile << endl;
+    cout << "Parameter file writen " << parFile << endl;
+    cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
+    cout << endl;
+    // ------------------------------------------------------------------------
 }
