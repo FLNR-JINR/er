7149b259b3e4b8ab5e0494e3a544ffeded0ee898
Mikhail Kozlov <kozlov.m.your@yandex.ru>
2017-10-17 22:51:54 +0300

    IonMixGenerator refactoring, Logger, CodeConvention

--- generators/ERIonMixGenerator.h
+++ generators/ERIonMixGenerator.h
@@ -1,6 +1,10 @@
-// -----                   ERIonMixGenerator header file                 -----
-// -----          		   Created   by            -----
-// -------------------------------------------------------------------------
+/********************************************************************************
+ *              Copyright (C) Joint Institute for Nuclear Research              *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
 
 #ifndef ERIonMixGenerator_H
 #define ERIonMixGenerator_H
@@ -15,56 +19,80 @@
 class FairIon;
 class FairPrimaryGenerator;
 
+/** @class ERIonMixGenerator
+ ** @brief Class for the generation different ions during one simulation.
+ ** @author M.Kozlov <kozlov.m.your@yandex.ru>
+ ** @version 1.0
+ **
+ ** The ERIonGenerator defines the parameters of primary ion and background ions 
+ ** which participates in simulation. It provides interfaces for determining  
+ ** different types of dimentional, angular and energy distributions 
+ ** at beam start position.
+ ** Also it allows to add background ions with certain probability of appearence.
+ ** Probability of primary ion is 1. For background ions it normalized relative
+ ** to the main ion.
+**/
 class ERIonMixGenerator : public ERIonGenerator
 {
 
-  public:
+	public:
 
-    /** Default constructor **/
-    ERIonMixGenerator();
+		/** Default constructor **/
+		ERIonMixGenerator();
 
-
-    /** Constructor with ion name
-      ** For the generation of ions with pre-defined FairIon
-     ** By default, the  excitation energy is zero. This can be changed with the
-     ** respective modifiers.
-     **@param ionName  Ion name
-     **@param mult      Number of ions per event
-     **@param px,py,pz  Momentum components [GeV] per nucleon!
-     **@param vx,vy,vz  Vertex coordinates [cm]
-     **/
-    ERIonMixGenerator(const Char_t* ionName, Int_t mult);
+		/** @brief Constructor with ion name
+		 ** For the generation of ions with pre-defined FairIon
+		 ** By default, the  excitation energy is zero. This can be changed with the
+		 ** respective modifiers inherited from ERIonGenerator.
+		 ** @param ionName  Ion name
+		 ** @param mult      Number of ions per event
+		**/
+		ERIonMixGenerator(const Char_t* ionName, Int_t mult);
 
 
-    /** Default constructor
-     ** For the generation of ions with atomic number z and mass number a.
-     ** By default, the mass equals a times the proton mass and the
-     ** excitation energy is zero. This can be changed with the
-     ** respective modifiers.
-     **@param z         Atomic number
-     **@param a         Atomic mass
-     **@param q         Electric charge [e]
-     **@param mult      Number of ions per event
-     **@param px,py,pz  Momentum components [GeV] per nucleon!
-     **@param vx,vy,vz  Vertex coordinates [cm]
-     **/
-    ERIonMixGenerator(TString name, Int_t z, Int_t a, Int_t q, Int_t mult);
+		/** @brief Constructor
+		 ** For the generation of primary ions with atomic number z and mass number a.
+		 ** By default, the mass equals a times the proton mass and the
+		 ** excitation energy is zero. This can be changed with the
+		 ** respective modifiers inherited from ERIonGenerator.
+		 ** @param z         Atomic number
+		 ** @param a         Atomic mass
+		 ** @param q         Electric charge [e]
+		 ** @param mult      Number of ions per event
+		**/
+		ERIonMixGenerator(TString name, Int_t z, Int_t a, Int_t q, Int_t mult);
 
-    /** Destructor **/
-    virtual ~ERIonMixGenerator();
+		/** @brief Destructor **/
+		virtual ~ERIonMixGenerator();
 
-    Bool_t ReadEvent(FairPrimaryGenerator* primGen);
-    void AddBackgroundIon(TString name, Int_t z, Int_t a, Int_t q, Double_t newIonProb);
+		/** Modifiers **/
+		
+		/** @brief Adds background ions with probability of appearance in simulation.
+		 ** Probability of primary ion is 1. For background ions it normalized relative
+		 ** to the main ion. For example, if we have primary ion with probability 0.5 and 
+ 		 ** two background ions with probabilities 0.2 and 0.3 we should pass argument
+ 		 ** newIonProb with \f$\frac{0.2}{0.5}\f$ and \f$\frac{0.3}{0.5}\f$ values respectively.
+		 ** @param z           Atomic number
+		 ** @param a           Atomic mass
+		 ** @param q           Electric charge [e]
+		 ** @param newIonProb  Probability of appearence relative to main ion
+		**/
+		void AddBackgroundIon(TString name, Int_t z, Int_t a, Int_t q, Double_t newIonProb);
 
-  private:
+		/** @brief Method ReadEvent
+		 ** Generates <mult> of the specified ions and hands hem to the
+		 ** FairPrimaryGenerator.
+		**/
+		Bool_t ReadEvent(FairPrimaryGenerator* primGen);
 
-    std::map<Double_t, TString>  fBgIons;   // Background ions names with emegence probability
-    Double_t                     fSumProbability;
-    
-    ERIonMixGenerator(const ERIonMixGenerator&);
-    ERIonMixGenerator& operator=(const ERIonMixGenerator&);
+	private:
+		std::map<Double_t, TString>  fBgIons;          ///< Background ion names with emegence probability
+		Double_t                     fSumProbability;  ///< Background ions summary probability
+		
+		ERIonMixGenerator(const ERIonMixGenerator&);
+		ERIonMixGenerator& operator=(const ERIonMixGenerator&);
 
-    ClassDef(ERIonMixGenerator,1)
+		ClassDef(ERIonMixGenerator,1)
 };
 
 
