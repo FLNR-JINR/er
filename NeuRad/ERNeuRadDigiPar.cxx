75b9f0ad044c40fe4694681fc960bc598ed3a302
evovch <evovch@gmail.com>
2017-11-17 18:00:26 +0300

    Cutify ERNeuRadDigiPar.

--- NeuRad/ERNeuRadDigiPar.cxx
+++ NeuRad/ERNeuRadDigiPar.cxx
@@ -1,19 +1,28 @@
+/********************************************************************************
+ *              Copyright (C) Joint Institute for Nuclear Research              *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
 #include "ERNeuRadDigiPar.h"
 
+#include "TString.h"
+#include "TMath.h"
+
 #include "FairParamList.h"
-#include "FairDetParIo.h"
+//#include "FairDetParIo.h"
 #include "FairParIo.h"
 #include "FairGenericParAsciiFileIo.h"
 #include "FairGenericParRootFileIo.h"
 #include "FairParAsciiFileIo.h"
 #include "FairParRootFileIo.h"
+#include "FairLogger.h"
 
-#include "TString.h"
-#include "TMath.h"
+#include "ERNeuRadSetup.h"
 
-#include <iostream>
-
-// -----   Standard constructor   ------------------------------------------
+// -----   Standard constructor   -------------------------------------------------------
 ERNeuRadDigiPar::ERNeuRadDigiPar(const char* name,
     const char* title,
     const char* context)
@@ -28,80 +37,80 @@ ERNeuRadDigiPar::ERNeuRadDigiPar(const char* name,
     fPixelCrosstalks(NULL),
     fFiberCrosstalks(NULL)
 {
-  std::cout << "ERNeuRadDigiPar" << std::endl;
+  LOG(INFO) << "ERNeuRadDigiPar constructor" << FairLogger::endl;
 }
 
-// -----   Destructor   ----------------------------------------------------
+// -----   Destructor   -----------------------------------------------------------------
 ERNeuRadDigiPar::~ERNeuRadDigiPar()
 {
   clear();
 }
 
-// -----   Public method clear   -------------------------------------------
+// -----   Public method clear   --------------------------------------------------------
 void ERNeuRadDigiPar::clear()
 {
   status = kFALSE;
   resetInputVersions();
 }
 
-// -----   Public method print ---------------------------------------
+// -----   Public method print ----------------------------------------------------------
 void ERNeuRadDigiPar::print()
 {
   fRowNofFibers = (Int_t)TMath::Sqrt(fNofPixels);
 
-  std::cout << "*****************************************" << std::endl;
-  std::cout << "          ERNeuRadDigiPar                " << std::endl;
-  std::cout << "*****************************************" << std::endl;
-  std::cout << "   ERNeuRadNofModules: " <<  fNofModules <<  std::endl;
-  std::cout << "   ERNeuRadNofPixels: " <<  fNofPixels <<  std::endl;
-  std::cout << "   ERNeuRadPixelQuantumEfficiency: " <<  std::endl;
-  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++){
-    std::cout << "     ";
+  LOG(INFO) << "*****************************************" << FairLogger::endl;
+  LOG(INFO) << "          ERNeuRadDigiPar                " << FairLogger::endl;
+  LOG(INFO) << "*****************************************" << FairLogger::endl;
+  LOG(INFO) << "   ERNeuRadNofModules: " << fNofModules << FairLogger::endl;
+  LOG(INFO) << "   ERNeuRadNofPixels: " << fNofPixels << FairLogger::endl;
+  LOG(INFO) << "   ERNeuRadPixelQuantumEfficiency: " << FairLogger::endl;
+  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++) {
+    LOG(INFO) << "     ";
     for (Int_t jFiber = 0; jFiber < fRowNofFibers; jFiber++)
-      std::cout <<(*fPixelQuantumEfficiency)[iFiber*fRowNofFibers + jFiber] << "\t";
-     std::cout << std::endl;
+      LOG(INFO) <<(*fPixelQuantumEfficiency)[iFiber*fRowNofFibers + jFiber] << "\t";
+     LOG(INFO) << FairLogger::endl;
   }
-  std::cout << "*****************************************" << std::endl;
+  LOG(INFO) << "*****************************************" << FairLogger::endl;
 
-  std::cout << "   ERNeuRadPixelGain: " <<  std::endl;
-  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++){
-    std::cout << "     ";
+  LOG(INFO) << "   ERNeuRadPixelGain: " << FairLogger::endl;
+  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++) {
+    LOG(INFO) << "     ";
     for (Int_t jFiber = 0; jFiber < fRowNofFibers; jFiber++)
-      std::cout <<(*fPixelGain)[iFiber*fRowNofFibers + jFiber] << "\t";
-     std::cout << std::endl;
+      LOG(INFO) <<(*fPixelGain)[iFiber*fRowNofFibers + jFiber] << "\t";
+     LOG(INFO) << FairLogger::endl;
   }
-  std::cout << "*****************************************" << std::endl;
+  LOG(INFO) << "*****************************************" << FairLogger::endl;
 
-  std::cout << "   ERNeuRadPixelSigma: " <<  std::endl;
-  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++){
-    std::cout << "     ";
+  LOG(INFO) << "   ERNeuRadPixelSigma: " << FairLogger::endl;
+  for (Int_t iFiber = 0; iFiber < fRowNofFibers; iFiber++) {
+    LOG(INFO) << "     ";
     for (Int_t jFiber = 0; jFiber < fRowNofFibers; jFiber++)
-      std::cout <<(*fPixelSigma)[iFiber*fRowNofFibers + jFiber] << "\t";
-     std::cout << std::endl;
+      LOG(INFO) <<(*fPixelSigma)[iFiber*fRowNofFibers + jFiber] << "\t";
+     LOG(INFO) << FairLogger::endl;
   }
 
-  std::cout << "*****************************************" << std::endl;
+  LOG(INFO) << "*****************************************" << FairLogger::endl;
 }
 
-//------------------------------------------------------
+//---------------------------------------------------------------------------------------
 void ERNeuRadDigiPar::putParams(FairParamList* l)
 {
 }
 
-//------------------------------------------------------
+//---------------------------------------------------------------------------------------
 Bool_t ERNeuRadDigiPar::getParams(FairParamList* l)
 {
-  std::cout << "ERNeuRadDigiPar Filling ..." << std::endl;
+  LOG(INFO) << "ERNeuRadDigiPar Filling ..." << FairLogger::endl;
 
   ERNeuRadSetup* setup = ERNeuRadSetup::Instance();
   fNofPixels = setup->RowNofPixels()*setup->RowNofPixels();
   fNofModules = setup->RowNofModules()*setup->RowNofModules();
 
-  std::cout << "fNofPixels " << fNofPixels << std::endl;
+  LOG(INFO) << "fNofPixels " << fNofPixels << FairLogger::endl;
 
   fPixelQuantumEfficiency = new TArrayF(fNofPixels);
   fPixelGain = new TArrayF(fNofPixels);
-  fPixelSigma = new TArrayF(fNofPixels),
+  fPixelSigma = new TArrayF(fNofPixels);
   fPixelCrosstalks = new TArrayF(fNofPixels*9);
   fFiberCrosstalks = new TArrayF(fNofPixels*9);
 
@@ -110,47 +119,48 @@ Bool_t ERNeuRadDigiPar::getParams(FairParamList* l)
   if ( ! l->fill("ERNeuRadPixelGain", fPixelGain) ) { return kFALSE; }
   if ( ! l->fill("ERNeuRadPixelSigma", fPixelSigma) ) { return kFALSE; }
   if ( ! l->fill("ERNeuRadPixelCrosstalks", fPixelCrosstalks) ) {
-    std::cerr << "ERNeuRadDigiPar: can`t find ERNeuRadPixelCrosstalks" << std::endl;
+    LOG(WARNING) << "ERNeuRadDigiPar: can`t find ERNeuRadPixelCrosstalks" << FairLogger::endl;
   } else {
     fUseCrosstalks = kTRUE;
   }
   if ( ! l->fill("ERNeuRadFiberCrosstalks", fFiberCrosstalks) ) {
-    std::cerr << "ERNeuRadDigiPar: can`t find ERNeuRadFiberCrosstalks" << std::endl;
+    LOG(WARNING) << "ERNeuRadDigiPar: can`t find ERNeuRadFiberCrosstalks" << FairLogger::endl;
   } else {
     fUseCrosstalks = kTRUE;
   }
 
   fPixelQuantumEfficiency->Set(fNofPixels);
   fPixelGain->Set(fNofPixels);
-  fPixelSigma->Set(fNofPixels),
+  fPixelSigma->Set(fNofPixels);
   fPixelCrosstalks->Set(fNofPixels*9);
   fFiberCrosstalks->Set(fNofPixels*9);
-  std::cout << "fPixelQuantumEfficiency " << fPixelQuantumEfficiency->GetSize() << std::endl;
+  LOG(INFO) << "fPixelQuantumEfficiency " << fPixelQuantumEfficiency->GetSize() << FairLogger::endl;
   return kTRUE;
 }
 
-//------------------------------------------------------
-Bool_t ERNeuRadDigiPar::init(FairParIo* input){
-  std::cout << input->getFilename() << std::endl;
-  if ( TString(input->getFilename()).Contains(".digi")){
-    FairGenericParAsciiFileIo* p=new FairGenericParAsciiFileIo(((FairParAsciiFileIo*)input)->getFile());
+//---------------------------------------------------------------------------------------
+Bool_t ERNeuRadDigiPar::init(FairParIo* input) {
+  LOG(INFO) << input->getFilename() << FairLogger::endl;
+  if ( TString(input->getFilename()).Contains(".digi")) {
+    FairGenericParAsciiFileIo* p = new FairGenericParAsciiFileIo(((FairParAsciiFileIo*)input)->getFile());
     return p->init(this);
   }
-  if ( TString(input->getFilename()).Contains(".root")){
-    FairGenericParRootFileIo* p=new FairGenericParRootFileIo(((FairParRootFileIo*)input)->getParRootFile());
+  if ( TString(input->getFilename()).Contains(".root")) {
+    FairGenericParRootFileIo* p = new FairGenericParRootFileIo(((FairParRootFileIo*)input)->getParRootFile());
     return p->init(this);
   }
   return kFALSE;
 }
 
-//------------------------------------------------------
+//---------------------------------------------------------------------------------------
 void ERNeuRadDigiPar::Crosstalks(Int_t iPixel, TArrayF& crosstalks) const {
-  //Возвращает матрицу три на три. Каждый элемент матрицы - кростолк к соответствующему соседу.
-  //Центральая ячейка - вероятность фотонов, которые останутся в волокне.
-  //Вне зависимости от того, что написано в файле параметров потом пересчитывается, чтобы суммарная вероятнсть была равна 1
+  // Возвращает матрицу три на три. Каждый элемент матрицы - кросс-толк к соответствующему соседу.
+  // Центральная ячейка - вероятность фотонов, которые останутся в волокне.
+  // Вне зависимости от того, что написано в файле параметров,
+  // потом пересчитывается, чтобы суммарная вероятнсть была равна 1.
   crosstalks.Set(9);
-  Int_t shift  = iPixel*9;
-  for (Int_t i = 0; i < 9; i++){
+  Int_t shift = iPixel*9;
+  for (Int_t i = 0; i < 9; i++) {
     crosstalks[i] = (*fPixelCrosstalks)[shift + i]+(*fFiberCrosstalks)[shift + i];
   }
 }
--- NeuRad/ERNeuRadDigiPar.h
+++ NeuRad/ERNeuRadDigiPar.h
@@ -1,3 +1,11 @@
+/********************************************************************************
+ *              Copyright (C) Joint Institute for Nuclear Research              *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+
 // -------------------------------------------------------------------------
 // -----                  ERNeuRadDigiPar header file                  -----
 // -----                  Created 03/15  by V.Schetinin                -----
@@ -6,75 +14,68 @@
 #ifndef ERNEURADDIGIPAR_H
 #define ERNEURADDIGIPAR_H
 
-#include "FairParGenericSet.h"
+#include "FairParGenericSet.h" // mother class
 
-#include "TObject.h"
-#include "TObjArray.h"
 #include "TArrayF.h"
-#include "TArrayI.h"
-
-#include "ERNeuRadSetup.h"
 
 class FairParIo;
 class FairParamList;
-class ERNeuRadSetup;
 
 class ERNeuRadDigiPar : public FairParGenericSet
 {
-
 public:
 
-    /** Standard constructor **/
-    ERNeuRadDigiPar(const char* name    = "ERNeuRadDigiPar",
-                            const char* title   = "ERNeuRad Digitization Parameters",
-                            const char* context = "Default");
+  /** Standard constructor **/
+  ERNeuRadDigiPar(const char* name    = "ERNeuRadDigiPar",
+                  const char* title   = "ERNeuRad Digitization Parameters",
+                  const char* context = "Default");
 
-    /** Destructor **/
-    virtual ~ERNeuRadDigiPar();
+  /** Destructor **/
+  virtual ~ERNeuRadDigiPar();
 
-    /** Initialisation from input device**/
-    virtual Bool_t init(FairParIo* input);
+  /** Initialization from input device **/
+  virtual Bool_t init(FairParIo* input);
 
-    /** Output to file **/
-    //virtual Int_t write(FairParIo* output);
+  /** Output to file **/
+  //virtual Int_t write(FairParIo* output);
 
-    virtual void print();
-    
-    /** Reset all parameters **/
-    virtual void clear();
+  virtual void print();
 
-    void putParams(FairParamList*);
-    Bool_t getParams(FairParamList*);
+  /** Reset all parameters **/
+  virtual void clear();
 
-    /** Modifiers**/
-    
-    /** Accessories  **/
-    Int_t   NofFibers()     const {return fNofPixels;}
-    Int_t   NofModules()    const {return fNofModules;}
-    //TODO ‰Ó·‡‚ËÚ¸ Ó·‡·ÓÚÍÛ ‚˚ıÓ‰‡ ÁÁ‡ „‡ÌËˆÛ
-    Double_t PixelQuantumEfficiency (Int_t iFiber) const {return (*fPixelQuantumEfficiency)[iFiber];}
-    Double_t PixelGain (Int_t iFiber) const {return (*fPixelGain)[iFiber];}
-    Double_t PixelSigma (Int_t iFiber) const {return (*fPixelSigma)[iFiber];}
-    Bool_t UseCrosstalks() const {return fUseCrosstalks;}
-    void Crosstalks(Int_t iFiber, TArrayF& crosstalks) const;
-    Int_t RowNofFibers() const {return fRowNofFibers;}
+  void putParams(FairParamList*);
+  Bool_t getParams(FairParamList*);
+
+  /** Modifiers**/
+
+  /** Accessories  **/
+  Int_t   NofFibers()     const {return fNofPixels;}
+  Int_t   NofModules()    const {return fNofModules;}
+  //TODO √§√Æ√°√†√¢√®√≤√º √Æ√°√∞√†√°√Æ√≤√™√≥ √¢√ª√µ√Æ√§√† √ß√ß√† √£√∞√†√≠√®√∂√≥
+  Double_t PixelQuantumEfficiency (Int_t iFiber) const {return (*fPixelQuantumEfficiency)[iFiber];}
+  Double_t PixelGain (Int_t iFiber) const {return (*fPixelGain)[iFiber];}
+  Double_t PixelSigma (Int_t iFiber) const {return (*fPixelSigma)[iFiber];}
+  Bool_t UseCrosstalks() const {return fUseCrosstalks;}
+  void Crosstalks(Int_t iFiber, TArrayF& crosstalks) const;
+  Int_t RowNofFibers() const {return fRowNofFibers;}
 
 private:
-    
-    TArrayF* fPixelQuantumEfficiency;
-    TArrayF* fPixelGain;
-    TArrayF* fPixelSigma;
-    TArrayF* fPixelCrosstalks;
-    TArrayF* fFiberCrosstalks;
-    Int_t fNofPixels;
-    Int_t fNofModules;
-    Bool_t fUseCrosstalks;
-    Int_t fRowNofFibers;
 
-    ERNeuRadDigiPar(const ERNeuRadDigiPar&);
-    ERNeuRadDigiPar& operator=(const ERNeuRadDigiPar&);
+  TArrayF* fPixelQuantumEfficiency;
+  TArrayF* fPixelGain;
+  TArrayF* fPixelSigma;
+  TArrayF* fPixelCrosstalks;
+  TArrayF* fFiberCrosstalks;
+  Int_t fNofPixels;
+  Int_t fNofModules;
+  Bool_t fUseCrosstalks;
+  Int_t fRowNofFibers;
 
-    ClassDef(ERNeuRadDigiPar,1)
+  ERNeuRadDigiPar(const ERNeuRadDigiPar&);
+  ERNeuRadDigiPar& operator=(const ERNeuRadDigiPar&);
+
+  ClassDef(ERNeuRadDigiPar, 1);
 };
 
 #endif // ERNEURADDIGIPAR_H
