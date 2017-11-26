7d1e53fa3b66270a758b75d12ef568fd85f912d5
Mikhail Kozlov <kozlov.m.your@yandex.ru>
2017-11-26 12:29:35 +0300

    Modified simulation macros

--- macro/QTelescope/QTelescope_sim.C
+++ macro/QTelescope/QTelescope_sim.C
@@ -1,97 +1,104 @@
-void QTelescope_sim(Int_t nEvents = 1000)
+void QTelescope_sim(Int_t nEvents = 10)
 {
-//---------------------Files-----------------------------------------------
-    TString outFile= "sim.root";
-    TString parFile= "par.root";
-// ------------------------------------------------------------------------
+  //---------------------Files-----------------------------------------------
+  TString outFile= "sim.root";
+  TString parFile= "par.root";
+  // ------------------------------------------------------------------------
 
-// -----   Timer   --------------------------------------------------------
-    TStopwatch timer;
-    timer.Start();
-// ------------------------------------------------------------------------
-// -----   Create simulation run   ----------------------------------------
-    FairRunSim* run = new FairRunSim();
+  // -----   Timer   --------------------------------------------------------
+  TStopwatch timer;
+  timer.Start(); 
+  // ----
+  // -----   Create simulation run   ----------------------------------------
+  FairRunSim* run = new FairRunSim();
 /** Select transport engine
 * TGeant3
 * TGeant4
 **/
-    run->SetName("TGeant4");              // Transport engine
-    run->SetOutputFile(outFile.Data());          // Output file
+  run->SetName("TGeant4");              // Transport engine
+  run->SetOutputFile(outFile.Data());          // Output file
 // ------------------------------------------------------------------------
 // -----   Runtime database   ---------------------------------------------
-    FairRuntimeDb* rtdb = run->GetRuntimeDb();
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
 // ------------------------------------------------------------------------
 // -----   Create media   -------------------------------------------------
-    run->SetMaterials("media.geo");       // Materials
+  run->SetMaterials("media.geo");       // Materials
 // ------------------------------------------------------------------------
 
-//-------- Set MC event header --------------------------------------------
-    ERMCEventHeader* header = new ERMCEventHeader();
-    run->SetMCEventHeader(header);
-//---------------------------------
-
-// -----   Create detectors  ----------------------------------------------
-    FairModule* cave= new ERCave("CAVE");
-    cave->SetGeometryFileName("cave.geo");
-    run->AddModule(cave);
-
-// Det definition
-/* Select verbosity level
- * 0 - only standard logs
- * 1 - Print points after each event
-*/
-    Int_t verbose = 1;
-    ERQTelescope* qtelescope= new ERQTelescope("ERQTelescope", kTRUE,verbose);
-    qtelescope->SetGeometryFileName("QTelescope.v1.geo.root");
-    run->AddModule(qtelescope);
-// ------------------------------------------------------------------------
-// -----   Create PrimaryGenerator   --------------------------------------
-    FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
-    Int_t pdgId = 2212; // proton  beam
-    Double32_t theta1 = -43.;  // polar angle distribution
-    Double32_t theta2 = 43.;  //ПОДОБРАТЬ ТЕТТА_1 и ТЕТТА_2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    Double32_t kin_energy = .500; //GeV
-    Double_t mass = TDatabasePDG::Instance()->GetParticle(pdgId)->Mass();
-    Double32_t momentum = TMath::Sqrt(kin_energy*kin_energy + 2.*kin_energy*mass); //GeV
-    FairBoxGenerator* boxGen = new FairBoxGenerator(pdgId, 1);
-    boxGen->SetThetaRange(theta1, theta2);
-    boxGen->SetPRange(momentum, momentum);
-    boxGen->SetPhiRange(0, 360); // ?????????????????????????????????????????????????????????????????????
-    boxGen->SetBoxXYZ(0.,0.,0.,0.,-100.);
-
-    primGen->AddGenerator(boxGen);
-    run->SetGenerator(primGen);
-// ------------------------------------------------------------------------
-//-------Set visualisation flag to true------------------------------------
-    run->SetStoreTraj(kTRUE);
-
-//-------Set LOG verbosity  -----------------------------------------------
-    FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
-
-// —– Initialize simulation run ————————————
-    run->Init();
-    Int_t nSteps = -15000;
-
-// —– Runtime database ———————————————
-    Bool_t kParameterMerged = kTRUE;
-    FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
-    parOut->open(parFile.Data());
-    rtdb->setOutput(parOut);
-    rtdb->saveOutput();
-    rtdb->print(); // ———————————————————
+  //-------- Set MC event header --------------------------------------------
+  ERMCEventHeader* header = new ERMCEventHeader();
+  run->SetMCEventHeader(header);
+  //---------------------------------
+  // -----   Create detectors  ----------------------------------------------
+  FairModule* cave= new ERCave("CAVE");
+  cave->SetGeometryFileName("cave.geo");
+  run->AddModule(cave);
+  Int_t verbose = 1;
+  ERQTelescope* qtelescope= new ERQTelescope("ERQTelescope", kTRUE,verbose);
+  ERQTelescopeSetup* qTelescopeSetup = ERQTelescopeSetup::Instance();
+  qTelescopeSetup->AddSi("DoubleSi1", 0, "X");
+  qTelescopeSetup->AddSi("DoubleSi1", 10, "X");
+  qTelescopeSetup->AddSi("SingleSi1", 100, "X");
+  qTelescopeSetup->AddSi("SingleSi1", 50, "Y");
+  qTelescopeSetup->AddSi("SingleSi2", 50, "X");
+  qTelescopeSetup->AddCsI("CsI1", -50);
+  qTelescopeSetup->AddCsI("CsI1",  25);
+  qTelescopeSetup->AddCsI("CsI1", -20);
 
 
-// -----   Run simulation  ------------------------------------------------
+  run->AddModule(qtelescope);
+
+ // FairModule* target = new ERTarget("BeamDetTarget", kTRUE, 1);
+  //target->SetGeometryFileName("target.h.geo.root");
+  //run->AddModule(target);
+  // ---------------.q
+  //--------------------------------------------------------
+  // -----   Create PrimaryGenerator   --------------------------------------
+  // -----   Create PrimaryGenerator   --------------------------------------
+  FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
+  Int_t pdgId = 2212; // proton  beam
+  Double32_t theta1 = -43.;  // polar angle distribution
+  Double32_t theta2 = 43.;  //ПОДОБРАТЬ ТЕТТА_1 и ТЕТТА_2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  Double32_t kin_energy = .500; //GeV
+  Double_t mass = TDatabasePDG::Instance()->GetParticle(pdgId)->Mass();
+  Double32_t momentum = TMath::Sqrt(kin_energy*kin_energy + 2.*kin_energy*mass); //GeV
+  FairBoxGenerator* boxGen = new FairBoxGenerator(pdgId, 1);
+  boxGen->SetThetaRange(theta1, theta2);
+  boxGen->SetPRange(momentum, momentum);
+  boxGen->SetPhiRange(0, 360); // ?????????????????????????????????????????????????????????????????????
+  boxGen->SetBoxXYZ(0.,0.,0.,0.,0.);
+
+  primGen->AddGenerator(boxGen);
+  run->SetGenerator(primGen);
+  // ------------------------------------------------------------------------
+  //-------Set visualisation flag to true------------------------------------
+  run->SetStoreTraj(kTRUE);
+
+  //-------Set LOG verbosity  -----------------------------------------------
+  FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
+  // —– Initialize simulation run ———————————— 
+  run->Init();
+  Int_t nSteps = -15000;
+
+  // —– Runtime database ——————————————— 
+  Bool_t kParameterMerged = kTRUE; 
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  parOut->open(parFile.Data()); 
+  rtdb->setOutput(parOut); 
+  rtdb->saveOutput(); 
+  rtdb->print(); // 
+  // -----   Run simulation  ------------------------------------------------
     run->Run(nEvents);
 
- // -----   Finish   -------------------------------------------------------
+    // -----   Finish   -------------------------------------------------------
     timer.Stop();
     Double_t rtime = timer.RealTime();
     Double_t ctime = timer.CpuTime();
     cout << endl << endl;
     cout << "Macro finished succesfully." << endl;
-    cout << "Output file is QTelescope_proton_sim.root" << endl;
-    cout << "Parameter file is QTelescope_proton_par.root" << endl;
+    cout << "Output file is sim.root" << endl;
+    cout << "Parameter file is par.root" << endl;
     cout << "Real time " << rtime << " s, CPU time " << ctime
-                         << "s" << endl << endl;
+                    << "s" << endl << endl;
+   // cout << "Energy " << momentum << "; mass " << mass << endl; 
 }
