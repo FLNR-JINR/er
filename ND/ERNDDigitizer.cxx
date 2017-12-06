4add4727c1af84fa703367a58cd9f88d7b338c41
SchetininVitaliy <sch_vitaliy@mail.ru>
2017-12-06 12:12:08 +0300

    fix ND digitizer error

--- ND/ERNDDigitizer.cxx
+++ ND/ERNDDigitizer.cxx
@@ -134,10 +134,10 @@ void ERNDDigitizer::Exec(Option_t* opt)
       }
     }
 
-    Float_t edepSigma = pow(fEdepErrorA,2) + pow(fEdepErrorB*TMath::Sqrt(edep/1000.),2) + pow(fEdepErrorC*edep/1000.,2);
+    Float_t edepSigma = sqrt(pow(fEdepErrorA,2) + pow(fEdepErrorB*TMath::Sqrt(edep/1000.),2) + pow(fEdepErrorC*edep,2));
     edep = gRandom->Gaus(edep, edepSigma);
 
-    Float_t lySigma = pow(fLYErrorA,2) + pow(fLYErrorB*TMath::Sqrt(ly/1000.),2) + pow(fLYErrorC*ly/1000.,2);
+    Float_t lySigma = sqrt(pow(fLYErrorA,2) + pow(fLYErrorB*TMath::Sqrt(ly/1000.),2) + pow(fLYErrorC*ly,2));
     ly = gRandom->Gaus(ly, lySigma);
 
     Float_t timeSigma = TMath::Sqrt(fTimeErrorA/ly);
