7c6669a58a1a16a46bab1bd63e3052d93d0d0b84
Mikhail Kozlov <kozlov.m.your@yandex.ru>
2018-02-27 11:25:13 +0300

    Added ERQTelescopeTrack

---
+++ telescope/data/ERQTelescopeTrack.h
@@ -0,0 +1,47 @@
+/********************************************************************************
+ *              Copyright (C) Joint Institute for Nuclear Research              *
+ *                                                                              *
+ *              This software is distributed under the terms of the             *
+ *         GNU Lesser General Public Licence version 3 (LGPL) version 3,        *
+ *                  copied verbatim in the file "LICENSE"                       *
+ ********************************************************************************/
+#ifndef ERQTelescopeTrack_H
+#define ERQTelescopeTrack_H
+
+#include "TNamed.h"
+
+/** @class ERQTelescopeTrackFinder
+ ** @brief 
+ ** @author M.Kozlov <kozlov.m.your@yandex.ru>
+ ** @version 1.0
+**/
+
+class ERQTelescopeTrack : public TNamed{
+
+public:
+  /** @brief Default constructor **/
+  ERQTelescopeTrack();
+
+  /** @brief Constructor 
+   ** @param targetX - hit x-coordinate on target;
+   ** @param targetY - hit y-coordinate on target;
+   ** @param targetZ - hit z-coordinate on target;
+   ** @param telescopeX - hit x-coordinate on QTelescope station;
+   ** @param telescopeY - hit y-coordinate on QTelescope station;
+   ** @param telescopeZ - hit z-coordinate on QTelescope station;
+  **/
+  ERQTelescopeTrack(Double_t targetX,    Double_t targetY,    Double_t targetZ, 
+                    Double_t telescopeX, Double_t telescopeY, Double_t telescopeZ);
+
+private:
+  Double_t  fTargetX;
+  Double_t  fTargetY;
+  Double_t  fTargetZ;                   // point coordinates on target
+  Double_t  fTelescopeX;
+  Double_t  fTelescopeY;
+  Double_t  fTelescopeZ;
+
+  ClassDef(ERQTelescopeTrack, 1)
+};
+
+#endif
