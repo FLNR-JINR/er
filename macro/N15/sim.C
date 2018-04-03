a845993f93979c8fb7bbcc78431236cd199789a4
evovch <evovch@gmail.com>
2018-04-03 17:56:47 +0300

    Cleanup.

--- macro/N15/sim.C
+++ macro/N15/sim.C
@@ -46,13 +46,13 @@ void sim(Int_t nEvents = 1000)
   run->AddModule(target);
 
   Int_t verbose = 0;
-
+/*
   ERDetector* detector= new ERDetector("TestDetector1", kTRUE,verbose);
   detector->SetGeometryFileName("N15.base.geo_1.root");
   detector->AddSensetive("gas1");
   detector->AddSensetive("plastic1");
   run->AddModule(detector);
-  
+*/
   //------    ER Decayer   -------------------------------------------------
   //Ion 15N
   Int_t A = 15;
@@ -68,8 +68,8 @@ void sim(Int_t nEvents = 1000)
   scattering->SetUniformPos(-0.00035,0.00035);
   scattering->SetStep(0.00001); //0.1 micron
   scattering->SetDecayVolume("targetB11");
-  scattering->SetThetaRange(23, 25);
-  scattering->SetPhiRange(0, 0);
+  scattering->SetThetaRange(23., 25.);
+  scattering->SetPhiRange(0., 0.);
 
   decayer->AddDecay(scattering);
   run->SetDecayer(decayer);
@@ -78,28 +78,28 @@ void sim(Int_t nEvents = 1000)
   FairPrimaryGenerator* primGen = new FairPrimaryGenerator();
 
   ERIonMixGenerator* generator = new ERIonMixGenerator("15N", Z, A, Q, 1);
-  Double32_t kin_energy = 0.043; //GeV
-  //generator->SetKinESigma(kin_energy, 0);
+  Double32_t kin_energy = 0.043; // GeV
   //generator->SetPSigma(6.7835, 6.7835*0.003);
-  generator->SetKinESigma(kin_energy, 0);
+  generator->SetKinESigma(kin_energy, 0.);
   generator->SpreadingOnTarget();
 
-  Double32_t theta = 0;
-  Double32_t sigmaTheta = 0.004*TMath::RadToDeg();
-  generator->SetThetaSigma(theta, 0);
-  generator->SetPhiRange(0, 360);
+  //Double32_t theta = 0.;
+  //Double32_t sigmaTheta = 0.004*TMath::RadToDeg();
+  generator->SetThetaSigma(0., 0.);
+  generator->SetPhiRange(0., 360.);
 
-  Double32_t distanceToTarget = 200;
+  Double32_t distanceToTarget = 200.;
   Double32_t sigmaOnTarget = 0.;
-  generator->SetSigmaXYZ(0, 0, -distanceToTarget, sigmaOnTarget, sigmaOnTarget);
-  generator->SetBoxXYZ(0,0,0,0, -distanceToTarget);
+  generator->SetSigmaXYZ(0., 0., -distanceToTarget, sigmaOnTarget, sigmaOnTarget);
+  generator->SetBoxXYZ(0.,0., 0.,0., -distanceToTarget); // Xmin, Xmax, Ymin, Ymax, Z
 
   //generator->AddBackgroundIon("26P", 15, 26, 15, 0.25);
   //generator->AddBackgroundIon("26S", 16, 26, 16, 0.25);
   //generator->AddBackgroundIon("24Si", 14, 24, 14, 0.25);
 
   primGen->AddGenerator(generator);
-/*
+
+/********************
   Int_t pdgId = 2212;
   Double32_t theta1 = 0.; // polar angle distribution
   Double32_t theta2 = 0.;
@@ -115,7 +115,8 @@ void sim(Int_t nEvents = 1000)
   boxGen->SetXYZ(0.,0., -100.);
 
   primGen->AddGenerator(boxGen);
-*/  
+********************/
+
   run->SetGenerator(primGen);
   // ------------------------------------------------------------------------
 
@@ -125,17 +126,18 @@ void sim(Int_t nEvents = 1000)
   //-------Set LOG verbosity  -----------------------------------------------
   FairLogger::GetLogger()->SetLogVerbosityLevel("LOW");
   FairLogger::GetLogger()->SetLogScreenLevel("DEBUG");
-  // —– Initialize simulation run ———————————— 
+
+  //------- Initialize simulation run ---------------------------------------
   run->Init();
   Int_t nSteps = -15000;
 
-  // —– Runtime database ——————————————— 
+  //--- Runtime database ----------------------------------------------------
   Bool_t kParameterMerged = kTRUE; 
   FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
   parOut->open(parFile.Data()); 
   rtdb->setOutput(parOut); 
   rtdb->saveOutput(); 
-  rtdb->print(); // 
+  rtdb->print();
 
   // -----   Run simulation  ------------------------------------------------
   run->Run(nEvents);
