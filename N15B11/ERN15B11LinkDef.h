3d82ea2cfbf28448f55db940d2859cabfed70d5e
satyshev <satyshev@jinr.ru>
2018-09-19 16:46:22 +0300

    solve conflicts

--- N15B11/ERN15B11LinkDef.h
+++ N15B11/ERN15B11LinkDef.h
@@ -5,7 +5,6 @@
 #pragma link off all functions;
 
 #pragma link C++ class ERN15B11Detector+;
-#pragma link C++ class ERN15B11ElasticScattering+;
 #pragma link C++ class ERN15B11Digitizer+;
 
 #endif // __CINT__
