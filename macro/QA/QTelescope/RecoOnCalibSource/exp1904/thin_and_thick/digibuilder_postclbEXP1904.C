ecfff986cb0a09ea41abaec0d98911bbfbd76494
komyour <kozlov.m.your@yandex.ru>
2019-11-30 17:25:24 +0300

    Report file for the SSD_20um_1 back side is added

--- macro/QA/QTelescope/RecoOnCalibSource/exp1904/thin_and_thick/digibuilder_postclbEXP1904.C
+++ macro/QA/QTelescope/RecoOnCalibSource/exp1904/thin_and_thick/digibuilder_postclbEXP1904.C
@@ -1,12 +1,4 @@
-TString getFileNameFromPath (TString const &path) {
-  TString fileName(path(path.Last('/') + 1, path.Length())); // file name with extension
-  // remove file extension. Extension is considered as part of the name after last "." symbol
-  fileName.Remove(fileName.Last('.'), fileName.Length());    
-  return fileName;
-}
-
-void digibuilder_postclbEXP1904(Int_t nEvents = 1443710)
-{
+void digibuilder_postclbEXP1904(Int_t nEvents = 1443710) {
   //TString intdir_converted = "/opt/keeper/stock/kozlov/calib201907/digibulder/alltel_90/";
   TString intdir_converted = "../input/";
   TString intdir_settings = "../input/";
@@ -27,23 +19,33 @@ void digibuilder_postclbEXP1904(Int_t nEvents = 1443710)
   // --- Source task
   ERDigibuilder* builder = new ERDigibuilder();
   builder->SetConfigurationFile(confFile);
-  //builder->SetUserCut("ERQTelescopeSiDigi_Telescope_1_SingleSi_SSD_1_X_1.fEdep>0.8e-3&&ERQTelescopeSiDigi_Telescope_1_SingleSi_SSD20_1_Y_0.fEdep > 0.8e-3");
-  //builder->SetUserCut("Telescope_1_SSD_1.fData.fValue>0.8e-3&&Telescope_1_SSD20_1.fData.fValue>0.8e-3");
   builder->AddFile(intdir_converted + rootAqqDaqFile);
 // -------------------------------------------------------------------------------------
+  ERTelescopeUnpack* telescope_1 = new ERTelescopeUnpack("Telescope_1");
   ERTelescopeUnpack* telescope_2 = new ERTelescopeUnpack("Telescope_2");
   // for the thin detector it nessesary to implement new mappin for pixelwise coefficients
-  telescope_2->AddSingleSiStation("SSD_2",
-                                  "SSD_2","tSSD_2",
-                                  intdir_settings + "SSD_1m_2.cal", "",
+  telescope_1->AddSingleSiStation("SSD_1",
+                                  "SSD_1", "tSSD_1",
+                                  intdir_settings + "SSD_1m_1.cal", "",
                                   "Y",
 				  kFALSE);
+  telescope_2->AddSingleSiStation("SSD_2",
+                                  "SSD_2", "tSSD_2",
+                                  intdir_settings + "SSD_1m_2.cal", "",
+                                  "X",
+				  kFALSE);
  // -------------------------------------------------------------------------------------  
-  telescope_2->AddSingleSiStation("SSD20_2",
-                                  "SSD20_2","tSSD20_2",
-                                  intdir_settings + "SSD_20u_2.cal", "",
+  telescope_1->AddSingleSiStation("SSD20_1",
+                                  "SSD20_1", "tSSD20_1",
+                                  intdir_settings + "SSD_20u_1.cal", "",
                                   "X",
                                   kFALSE);
+  telescope_2->AddSingleSiStation("SSD20_2",
+                                  "SSD20_2", "tSSD20_2",
+                                  intdir_settings + "SSD_20u_2.cal", "",
+                                  "Y",
+                                  kFALSE);
+  builder->AddUnpack(telescope_1);
   builder->AddUnpack(telescope_2);
   // --- Run
   FairRunOnline *run = new FairRunOnline(builder);
