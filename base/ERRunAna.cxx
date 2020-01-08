cfa67c5580f306d474b6c4f512b4bee0ab015496
Mikhail Kozlov <kozlov.m.your@yandex.ru>
2020-01-08 07:11:22 +0300

    Normal algorithm for losses estimation by EmCalculator

--- base/ERRunAna.cxx
+++ base/ERRunAna.cxx
@@ -48,6 +48,97 @@ fHoldEventsCount(kFALSE)
 {
   LOG(DEBUG) << "ERRunAna constructor" << FairLogger::endl;
 }
+
+double fDivision = 1000;
+
+bool AreSame(double a, double b, double eps)
+{
+    // cout << "a " << a << " b " << b << " eps " << eps << endl;
+    return fabs(a - b) < eps;
+}
+//--------------------------------------------------------------------------------------------------
+Double_t CalcElossIntegralVolStepGet (Double_t T, const G4ParticleDefinition* ion, 
+                                      const G4Material* mat, const Double_t range) { 
+  //FIXME copy-past from ERBeamDetSetup
+  assert(mat);
+  assert(ion);
+  if (range <= 0.)
+    return 0;
+  Double_t integralEloss = 0.;
+  const Double_t intStep = 0.009; // range / fDivision;
+  Double_t curStep = 0.;
+  G4EmCalculator* calc = new G4EmCalculator();
+  while (!AreSame(curStep, range, intStep / 2.)) {
+    Double_t eloss = calc->GetDEDX(T,ion,mat)*intStep*1e-4*10;
+    integralEloss += eloss;
+    T += eloss;
+    curStep += intStep;
+  }
+  return integralEloss;
+}//--------------------------------------------------------------------------------------------------
+Double_t CalcElossIntegralVolStepCompute (Double_t T, G4ParticleDefinition* ion, 
+                                          G4Material* mat, Double_t range) 
+{ 
+  //FIXME copy-past from ERBeamDetSetup
+  assert(mat);
+  assert(ion);
+  if (range <= 0.)
+    return 0;
+  Double_t integralEloss = 0.;
+  const Double_t intStep = 0.009; // range / fDivision;
+  Double_t curStep = 0.;
+  G4EmCalculator* calc = new G4EmCalculator();
+  while (!AreSame(curStep, range, intStep / 2.)) {
+    Double_t eloss = calc->ComputeDEDX(T,ion,"ionIoni",mat)*intStep*1e-4*10;
+    integralEloss += eloss;
+    T += eloss;
+    curStep += intStep;
+  }
+  // cout << "range " << range << " | curStep " << curStep << endl;
+  return integralEloss;
+}
+//--------------------------------------------------------------------------------------------------
+Double_t CalcElossIntegralVolStepGetInvDir (Double_t T, const G4ParticleDefinition* ion, 
+                                      const G4Material* mat, const Double_t range) { 
+  //FIXME copy-past from ERBeamDetSetup
+  assert(mat);
+  assert(ion);
+  if (range <= 0.)
+    return 0;
+  Double_t integralEloss = 0.;
+  const Double_t intStep = 0.009; // range / fDivision;
+  Double_t curStep = 0.;
+  G4EmCalculator* calc = new G4EmCalculator();
+  while (!AreSame(curStep, range, intStep / 2.)) {
+    Double_t eloss = calc->GetDEDX(T,ion,mat)*intStep*1e-4*10;
+    integralEloss += eloss;
+    T -= eloss;
+    curStep += intStep;
+  }
+  return integralEloss;
+}//--------------------------------------------------------------------------------------------------
+Double_t CalcElossIntegralVolStepComputeInvDir (Double_t T, G4ParticleDefinition* ion, 
+                                          G4Material* mat, Double_t range) 
+{ 
+  //FIXME copy-past from ERBeamDetSetup
+  assert(mat);
+  assert(ion);
+  if (range <= 0.)
+    return 0;
+  Double_t integralEloss = 0.;
+  const Double_t intStep = 0.009; // range / fDivision;
+  // cout << "intStep " << intStep << endl;
+  Double_t curStep = 0.;
+  G4EmCalculator* calc = new G4EmCalculator();
+  while (!AreSame(curStep, range, intStep / 2.)) {
+    Double_t eloss = calc->ComputeDEDX(T,ion,"ionIoni",mat)*intStep*1e-4*10;
+    integralEloss += eloss;
+    T -= eloss;
+    curStep += intStep;
+  }
+  // cout << "range " << range << " | curStep " << curStep << endl;
+  return integralEloss;
+}
 //--------------------------------------------------------------------------------------------------
 void ERRunAna::Init(){ 
   
@@ -88,150 +179,47 @@ void ERRunAna::Init(){
   geant4->ProcessRun(0);
 
 
-  // Double_t range = 8e-3;
-  // Double_t T = 7.68690;
-  // Double_t integralEloss = 0.;
-  // Double_t intStep = range / 1e1;
-  // Double_t curStep = 0.;
+  cout << "ERRunAna integration " << endl; 
+  G4IonTable* ionTable = G4IonTable::GetIonTable();
+  G4ParticleDefinition* ion =  ionTable->GetIon(1000020040);
+  G4EmCalculator* calc = new G4EmCalculator();
+  G4NistManager* nist = G4NistManager::Instance();
+  G4Material* mat = nist->FindOrBuildMaterial("silicon");
+  double range = 1e-4;
+  double T = 7.68690;
+  std::vector<double> energies = {4.7844, 6.0024, 7.6869}; // [MeVs]
+  // std::vector<double> energies = {4.7844}; // [MeVs]
+  // std::vector<double> thickCalc_dE = {2, 4, 6, 8, 12, 16, 20, 24, 28, 32, 36, 40}; // [um]
+  // std::vector<double> thickCalc_dE = {8, 12, 16, 20, 24, 28, 32, 36}; // [um]
+  std::vector<double> thickCalc_dE = {1, 2, 3, 4, 5, 6}; // [um]
+  // std::vector<double> thickCalc_dE  = {2, 4, 5, 5.56}; // [um]
+  cout << "d | edep_comp | edep_get " << endl;
+  // for (double div = 500; div <= 5000; div *= 10) {
+  //   fDivision = div;
+    for (auto itEnergies: energies) {
+      cout << itEnergies << endl;
+      for (auto itThickness: thickCalc_dE) {
+        double edep_comp_inv = CalcElossIntegralVolStepComputeInvDir(itEnergies, ion, mat, itThickness); 
+        double edep_get_inv = CalcElossIntegralVolStepGetInvDir(itEnergies, ion, mat, itThickness); 
+        double T_comp_after_dead = itEnergies - edep_comp_inv;
+        double T_get_after_dead = itEnergies - edep_get_inv;
+        // cout << itThickness << " & " << edep_comp_inv << " & " << edep_get_inv
+        //      << " & " << edep_comp_inv - edep_get_inv
+        //      << " \\\\ " << endl
+        //      << "\\hline" << endl;
+        cout << itThickness << " " << edep_comp_inv << " " << T_comp_after_dead << endl;
 
-  // G4IonTable* ionTable = G4IonTable::GetIonTable();
-  // G4ParticleDefinition* ion = ionTable->GetIon(1000020040);
-  // G4NistManager* nist = G4NistManager::Instance();
-  // G4Material* mat = nist->FindOrBuildMaterial("silicon");
 
-  // G4EmCalculator* calc = new G4EmCalculator();
-  // while (curStep <= range) {
-  //   Double_t eloss = calc->GetDEDX(T,ion,mat)*intStep;
-  //   integralEloss += eloss;
-  //   T -= eloss;
-  //   curStep += intStep;
+        double edep_comp = CalcElossIntegralVolStepCompute(T_comp_after_dead, ion, mat, itThickness); 
+        double edep_get = CalcElossIntegralVolStepGet(T_get_after_dead, ion, mat, itThickness); 
+        double T_restored_comp = T_comp_after_dead + edep_comp;
+        double T_restored_get = T_get_after_dead + edep_get;
+        cout << (itEnergies - T_restored_comp)*1e3 << " | " << (itEnergies - T_restored_get)*1e3 << endl;
+        // cout << (itEnergies - T_restored_comp)*1e3 << " | " << (itEnergies - T_restored_get)*1e3 << endl;
+
+      }
+    }  
   // }
-  // cout << "integralEloss " << integralEloss << endl;
-cout << "ERRunAna integration " << endl; 
-G4IonTable* ionTable = G4IonTable::GetIonTable();
-G4ParticleDefinition* ion =  ionTable->GetIon(1000020040);
-G4EmCalculator* calc = new G4EmCalculator();
-G4NistManager* nist = G4NistManager::Instance();
-G4Material* mat = nist->FindOrBuildMaterial("silicon");
-double range = 1e-4;
-double T = 7.68690;
-std::vector<double> energies = {4.7844, 6.0024, 7.6869}; // [MeVs]
-// std::vector<double> thickCalc_E  = {2, 4, 5, 5.56}; // [um]
-// std::vector<double> thickCalc_dE = {2, 4, 5, 5.56, 8, 12, 16, 20, 24, 28, 32, 36, 40}; // [um]
-// std::vector<double> thickCalc_dE = {2, 4, 6, 8, 12, 16, 20, 24, 28, 32, 36, 40}; // [um]
-// std::vector<double> thickCalc_dE = {8, 12, 16, 20, 24, 28, 32, 36}; // [um]
-std::vector<double> thickCalc_dE = {1, 2, 3, 4, 5, 6}; // [um]
-// std::vector<double> thickCalc_dE ={2.27}; // [um]
-// std::vector<double> thickCalc_dE  = {2, 4, 5, 5.56}; // [um]
-
-// for (auto itEnergies: energies) {
-//   double curStep = 0.;  // [um]
-//   double energy = itEnergies;
-//   double curDE = 0;
-//   cout << "itEnergies " << itEnergies << endl;
-//   auto itThickCalc_dE = thickCalc_dE.begin();
-//   while (itThickCalc_dE != thickCalc_dE.end()) {
-//     Double_t edep = calc->GetDEDX(energy,ion,mat)*intStep*1e-4*10;
-//     curDE += edep;
-//     energy -= edep;
-//     curStep += intStep;
-//     if (curStep >= *itThickCalc_dE) {
-//       itThickCalc_dE++;
-//       cout << curDE << " ";
-//     }
-//   } 
-//   cout << endl;
-// }  
-double intStep;// = 2. / 100.; // [um]
-for (auto itEnergies: energies) {
-  double curStep = 0.;  // [um]
-  double energy = itEnergies;
-  double energy_comp = itEnergies;
-  double curDE = 0;
-  double curDE_comp = 0;
-  cout << itEnergies << endl;
-  auto itThickCalc_E = thickCalc_dE.begin();
-  intStep = *itThickCalc_E / 1e3;
-  while (itThickCalc_E != thickCalc_dE.end()) {
-    Double_t edep = calc->GetDEDX(energy,ion,mat)*intStep*1e-4*10;
-    Double_t edep_comp = calc->ComputeDEDX(energy,ion,"ionIoni",mat)*intStep*1e-4*10;
-    curDE += edep;
-    curDE_comp += edep_comp;
-    energy_comp -= edep_comp;
-    energy -= edep;
-    curStep += intStep;
-    if (curStep > *itThickCalc_E) {
-      // cout << *itThickCalc_E 
-      //      << " & " << curDE 
-      //      << " & " << curDE_comp 
-      //      << " & " << (curDE - curDE_comp)*1e3 
-      //      << " \\\\"
-      //      << endl << "\\hline" << endl;
-      itThickCalc_E++;
-      cout << energy << " "  << curDE << " | " << energy_comp << " " << curDE_comp << endl;
-      // cout << energy << " " << curDE << endl;
-      // cout << itEnergies - energy_comp << endl;
-      curDE = 0;
-      curDE_comp = 0;
-      curStep = 0.;
-      energy = itEnergies;
-      energy_comp = itEnergies;
-      intStep = *itThickCalc_E / 1e3;
-    }
-  }
-  cout << endl;
-}  
-// double energy4H = 7.6869;
-// std::vector<double> dEdx;
-// std::vector<double> E;
-// intStep = 1e-3;
-// while (energy4H > 0) {
-//   Double_t edep = calc->GetDEDX(energy4H,ion,mat)*1e-3;
-//   dEdx.push_back(edep);
-//   E.push_back(energy4H);
-//   cout << edep << " " << energy4H << endl;
-//   energy4H -= 0.1;
-// }
-// cout << endl;
-
-// auto canv = new TCanvas();
-// auto gr = new TGraph(dEdx.size(), &E[0], &dEdx[0]);
-// gr->SetLineColor(kBlue);
-// gr->SetMarkerColor(kBlue);
-// gr->SetLineWidth(2);
-// gr->SetMarkerSize(1.5);
-// gr->SetMarkerStyle(21);
-// gr->SetName("Bethe-Bloch");
-// gr->SetTitle("Bethe-Bloch Alpha");
-// gr->Draw("APL");
-// gr->GetXaxis()->SetTitle("E,[MeV]");
-// gr->GetYaxis()->SetTitle("dE/dx,[MeV/um]");
-// canv->SaveAs("BetheBloch.root");
-// for (auto itEnergies: energies) {
-//   cout << "E = " << itEnergies << endl;
-//   for (auto it_thick_E: thickCalc_E) {
-//     Double_t edep = calc->GetDEDX(itEnergies,ion,mat)*it_thick_E*1e-4*10;
-//     cout << itEnergies - edep <<  " ";
-//   }
-//   cout << endl << endl;
-//   for (auto it_thick_dE: thickCalc_dE) {
-//     Double_t edep = calc->GetDEDX(itEnergies,ion,mat)*it_thick_dE*1e-4*10;
-//     cout << edep << " ";
-//   }
-//   cout << endl << endl;    
-//   for (auto it_thick_dE: thickCalc_E) {
-//     Double_t edep = calc->GetDEDX(itEnergies,ion,mat)*it_thick_dE*1e-4*10;
-//     Double_t edep_compute = calc->ComputeDEDX(itEnergies,ion,"",mat)*it_thick_dE*1e-4*10;
-//     cout << "edep_compute " << edep_compute << endl;
-//     cout << "| " << edep - edep_compute << " |; ";
-//   }
-//   cout << endl;
-//   cout << "===================" << endl;
-// }
-
-// std::cout << endl << "=====" << endl << "edep " << edep << std::endl;
-// exit(0);
-// exit(0);  
   exit(1);
 }
 
