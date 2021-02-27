f4f81bb659d4a778bf95ada04981631f179b7128
Vitaliy Schetinin <sch_vitaliy@mail.ru>
2021-02-27 12:08:58 +0300

    new digibuilder: first step

--- beamtime/ERDigibuilder.h
+++ beamtime/ERDigibuilder.h
@@ -18,56 +18,32 @@
 
 #include "ERUnpack.h"
 
-class ERDigibuilder : public FairSource
-{
-  public:
-    ERDigibuilder();
-    ERDigibuilder(const ERDigibuilder& source);
-    virtual ~ERDigibuilder();
-
-    virtual Bool_t Init();
-
-    virtual Int_t ReadEvent(UInt_t=0);
-
-    virtual void Close();
-
-    virtual void Reset();
-
-    virtual Source_Type GetSourceType(){return kFILE;}
-
-    virtual void SetParUnpackers(){}
-
-    virtual Bool_t InitUnpackers();
-
-    virtual Bool_t ReInitUnpackers(){return kTRUE;}
-
-    void AddUnpack(ERUnpack* unpack){ fUnpacks[unpack->GetDetName()] = unpack; }
-
-    void SetConfigurationFile(TString name){fSetupFile = name;}
-    void AddFile(TString path){fPath.push_back(path);}
-    void SetUserCut(TCut cut,Bool_t fillSkippedEvents=kTRUE);
-  private:
-    std::map<TString, ERUnpack*> fUnpacks;
-
-    // Вектор lmd файлов для обработки.
-    std::vector<TString> fPath;
-    // Индекс текущего файла в fPath.
-    Int_t fCurFile;
-    // Количество событий обработанных в предыдущих файлах
-    Int_t fOldEvents;
-
-    TString fSetupFile;
-    SetupConfiguration* fSetupConfiguration;
-    Reader* fReader;
-
-    TCut fUserCut;
-    Bool_t fFillSkippedEvents;
-    TH1I* fEventsForProcessing;
-
-    void DumpRawToScreen(DetEventDetector* det);
-    Int_t OpenNextFile();
-  public:
-    ClassDef(ERDigibuilder, 1)
+class ERDigibuilder : public FairSource {
+ public:
+  ERDigibuilder() = default;
+  virtual Bool_t Init();
+  virtual Int_t ReadEvent(UInt_t=0);
+  virtual void Close();
+  virtual void Reset();
+  virtual Source_Type GetSourceType() { return kFILE; }
+  virtual void SetParUnpackers() {}
+  virtual Bool_t InitUnpackers();
+  virtual Bool_t ReInitUnpackers() { return kTRUE; }
+  void AddUnpack(ERUnpack* unpack){ unpacks_[unpack->GetDetName()] = unpack; }
+  void AddFile(const TString& path) { file_paths_.push_back(path); }
+  void SetUserCut(const TCut& cut, bool fill_skipped_events = true);
+ private:
+  std::map<TString, ERUnpack*> unpacks_;
+  std::vector<TString> file_paths_;
+  uint current_file_index_ = 0;
+  uint events_count_in_already_processed_files_ = 0;
+  SetupConfiguration* setup_configuration_ = nullptr;
+  TCut user_cut_;
+  bool fill_skipped_events_ = true;
+  TH1I* events_for_processing_ = nullptr;
+  //void DumpRawToScreen(DetEventDetector* det);
+  Int_t OpenNextFile();
+  ClassDef(ERDigibuilder, 1)
 };
 
 #endif
